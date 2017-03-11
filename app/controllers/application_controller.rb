2feca5c7a7e53bcf8ba459517224db5b1945425a
Chris Holland <chris.holland987@gmail.com>
2017-03-10 16:16:49 -0800

    Sign-In Feature Created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
-  # before_action :authenticate_admin!
+  before_action :authenticate_user!
 
-  private
-  def require_sign_in
-    unless current_user
-      flash[:alert] = "You must be logged in to do that"
-      redirect_to new_session_path
-    end
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310224154) do
+ActiveRecord::Schema.define(version: 20170310235428) do
 
-  create_table "admins", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20170310224154) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
